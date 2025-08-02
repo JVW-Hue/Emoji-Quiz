19f018b6cbec175554605e91e00c2e22ce0a4aa7
JVW-Hue <janovanwyk553@gmail.com>
2025-08-02 17:26:57 +0200

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import os
-import django
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emoji_quiz.settings')
-django.setup()
-
-app = get_wsgi_application()
